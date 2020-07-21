21ee321f7021f0cc1999591a73aecfbbd71ebec0
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-21 16:54:51 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -12,7 +12,7 @@ class Backer
 
   def back_project(project)
     backed_projects << self
-    project.add_backer(self)
+    project.add_backer(self) unless project.backers.include?(self)
   end
 
 end
